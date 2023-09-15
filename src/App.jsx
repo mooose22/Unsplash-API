5d2db2416ab0ac98b45979884156e61ad054fd53
mooose22 <mostafamoawad22@gmail.com>
2023-09-15 19:45:06 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ThemeToggle from "./ThemeToggle";
+import SearchForm from "./SearchForm";
+import Galley from "./Gallery";
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Galley />
+    </main>
+  );
+};
+export default App;
---
+++ src/context.jsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const [searchTerm, setSearchTerm] = useState("water");
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark-theme", newDarkTheme);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
